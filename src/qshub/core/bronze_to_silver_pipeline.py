8be18a8c01ea8013deaa1b1cfb55bd4e7cb5ed0d
Warren Davies <warren_davies81@hotmail.com>
2026-03-31 12:12:51 +0100

    added BronzeToSilverPipeline

---
+++ src/qshub/core/bronze_to_silver_pipeline.py
@@ -0,0 +1,10 @@
+from qshub.core.pipeline import BasePipeline
+
+
+class BronzeToSilverPipeline(BasePipeline):
+
+    def __init__(self, steps):
+        super().__init__(steps)
+
+    def get_new_files():
+        pass
