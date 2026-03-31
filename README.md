ff262ea756ba6ade0645f9548b5e6b8718f312e1
Warren Davies <warren_davies81@hotmail.com>
2026-03-31 12:13:32 +0100

    added BasePipeline

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # qs-hub
 Quantified self data integration and analysis tools.
+
+
+# Plan
+* Get this example pipeline working and merged 
+* Sort idempotency - example.csv and example2.csv - importing example.csv twice should - create a new timestampted file in bronze, but the import process to silver should have no effect. example2.csv will have the same as example, but one extra row. Just that extra row should be imported. 
+* Then in my fork I create the real pipeline for the daily tracker 
+* Then finally get to practicing making machine learning models
--- example.py
+++ example.py
@@ -1,10 +1,19 @@
 from qshub.main import main
 from qshub.utils import utils
 from qshub.ingestion import local_files
+from qshub.core.storage_manager import StorageManager
 
+from pipelines.registry import pipeline_registry
+
+### Storage
+storage = StorageManager()
+
+
+### INGESTION
 
 ingestion_config_files = utils.get_yaml_files_in_dir("./configs/ingestion/")
 ingestion_config_files = [utils.load_yaml(file) for file in ingestion_config_files]
+
 for ingestion_config_file in ingestion_config_files:
     for run in ingestion_config_file["runs"]:
         local_files.collect_local_file(
@@ -12,6 +21,13 @@ for ingestion_config_file in ingestion_config_files:
             run["destination_path"],
         )
 
-# pipeline_configs = utils.get_yaml_files_in_dir(config_path + "/pipelines")
-# pipeline_configs = [utils.yaml_to_dict(file) for file in pipeline_configs]
 
+### BRONZE TO SILVER
+
+bronze_to_silver_config = utils.load_yaml("./configs/pipelines/bronze-to-silver.yaml")
+
+for run in bronze_to_silver_config["runs"]:
+    pipeline = pipeline_registry[run["name"]]
+    df = storage.read(run["source_path"])
+    df_transformed = pipeline.run(df)
+    storage.write(run["destination_path"], df_transformed)
