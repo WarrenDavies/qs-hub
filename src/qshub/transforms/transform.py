9cc6721d5937368c4f9d3fbce64be91cf30f9fab
Warren Davies <warren_davies81@hotmail.com>
2026-03-31 12:10:28 +0100

    added transform.py

---
+++ src/qshub/transforms/transform.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Callable, Dict, Any
+import pandas as pd
+
+@dataclass
+class Transform:
+    func: Callable[[pd.DataFrame, Dict[str, Any]], pd.DataFrame]
+    params: Dict[str, Any] = None
+    name: str = None
+
+    def run(self, df: pd.DataFrame) -> pd.DataFrame:
+        return self.func(df, **(self.params or {}))
